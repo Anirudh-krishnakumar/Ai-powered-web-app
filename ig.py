dc73318f44eff052d990a1679154843638d61e8e
Anirudh <165251969+Anirudh-krishnakumar@users.noreply.github.com>
2024-08-22 20:03:26 +0530

    Add files via upload

---
+++ ig.py
@@ -0,0 +1,21 @@
+
+import streamlit as st
+
+import requests
+st.title("Anik_Ai")
+a=st.text_input("enter text")
+API_URL = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"
+headers = {"Authorization":  "Bearer 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"}
+
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.content
+image_bytes = query({
+	"inputs": a,
+})
+# You can access the image with PIL.Image for example
+import io
+from PIL import Image
+image = Image.open(io.BytesIO(image_bytes))
+st.image(image)
